55a6620da9f561e7fe9127d9d1c73ff17e148529
Sofía Tetzlaff <sofiatetzlaff@gmail.com>
2023-10-17 23:02:20 -0300

    Tech proof - v0.2
    Volver a las desde los minijuegos a las zonas necesarias, anim botón minijuego

--- Sendero_Naumi_Project/Scenes/Zone.gd
+++ Sendero_Naumi_Project/Scenes/Zone.gd
@@ -1,11 +1,11 @@
-extends Sprite2D
+extends TextureRect
 var completeZone 
 @export var NumOfMinigame : int
 
-func _on_btn_zone_pressed():
-	if !completeZone:
-		get_node("../Camera2D").zoom_in(position - get_node("../Camera2D").position)
-		get_node("In_Zone").visible = true;
+#func _on_btn_zone_pressed():
+	#if !completeZone:
+	#	get_node("../Camera2D").zoom_in(position - get_node("../Camera2D").position)
+	#	get_node("In_Zone").visible = true;
 
 
 func ZoneComplete():
