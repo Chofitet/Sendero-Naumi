ec2378482347e38c0b2b72408592002bc497ab74
Sofía Tetzlaff <sofiatetzlaff@gmail.com>
2025-01-21 10:37:27 -0300

    adjusts

--- Sendero_Naumi_Project/_Scripts/MadeLineWithPath.gd
+++ Sendero_Naumi_Project/_Scripts/MadeLineWithPath.gd
@@ -1,26 +1,27 @@
 @tool
 extends Path2D
+var verticalValue
 
-@export var anchorMegafauna : Node2D
-@export var anchorHistoria : Node2D
-@export var anchorGeologia: Node2D
-@export var anchorAstronomia : Node2D
-@export var anchormegafauna2 : Node2D
-@export var anchorErasGeologicas : Node2D
-@export var anchorBiodiversidad : Node2D
-
-func _process(delta):
-	if !Engine.is_editor_hint(): return
-	curve.set_point_position(0,anchorMegafauna.global_position)
-	curve.set_point_position(3,anchorHistoria.global_position)
-	curve.set_point_position(4,anchorGeologia.global_position)
-	curve.set_point_position(6,anchorAstronomia.global_position)
-	curve.set_point_position(11,anchormegafauna2.global_position)
-	curve.set_point_position(12,anchorErasGeologicas.global_position)
-	curve.set_point_position(13,anchorBiodiversidad.global_position)
-	var pathPoints = curve.get_baked_points()
-	$Line2D.points = pathPoints
+@onready var line = $Line2D
+@export var AnchorsIslands :=[Control]
+@export var HasAnchors : bool
+var NumOfPoint  = [0,3,4,6,11,12,13] 
 
 func _ready():
-	var pathPoints = curve.get_baked_points()
-	$Line2D.points = pathPoints
+	if Engine.is_editor_hint() : return
+	verticalValue = get_parent().get_parent().size.y + get_viewport_rect().size.y/2
+	get_parent().position.y = verticalValue
+
+func _process(delta):
+	if !Engine.is_editor_hint() : return
+	PositionSparcklePath()
+
+func PositionSparcklePath():
+	var i = 0
+	if HasAnchors:
+		for _points in NumOfPoint:
+			curve.set_point_position(_points,get_node(AnchorsIslands[i]).global_position)
+			i += 1
+
+	
+	line.points = curve.get_baked_points()
