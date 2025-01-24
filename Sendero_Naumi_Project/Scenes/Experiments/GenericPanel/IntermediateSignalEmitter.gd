307cba678dd1847aafde4746b22f39350077926d
Sofía Tetzlaff <sofiatetzlaff@gmail.com>
2025-01-24 11:30:31 -0300

    Restart busqueda
    Restart busqueda

--- Sendero_Naumi_Project/Scenes/Experiments/GenericPanel/IntermediateSignalEmitter.gd
+++ Sendero_Naumi_Project/Scenes/Experiments/GenericPanel/IntermediateSignalEmitter.gd
@@ -3,10 +3,12 @@ class_name  _IntermediateSignal
 extends Node
 signal ButtonPress
 @export var Data : IntermediateData
+@export var Delay : float
 
 func EmitSignal(numOfPanel):
 	if Data.NumberOfPanel - 1 == numOfPanel: 
 		print("Signal of panel " + str(Data.NumberOfPanel))
+		await get_tree().create_timer(Delay).timeout
 		ButtonPress.emit()
 
 func ConnectSignal():
